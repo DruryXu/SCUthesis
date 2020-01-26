b1eed891f927f0a468a920f0ca87828215439c67
DruryXu <xujunzhedrury@outlook.com>
2020-01-27 06:11:48 +0800

    usercf

--- main.py
+++ main.py
@@ -20,40 +20,50 @@ if __name__ == "__main__":
 #
 #    with open('positive.json', "w", encoding='utf-8') as f:
 #        f.write(json.dumps(positive))
-    with open('positive.json', 'r', encoding='utf-8') as f:
-        positive = json.loads(f.read())
-    
-    similarity, c  = defaultdict(int), defaultdict(int)
-    target_user = 19
-    targetNum = 0
-    for k,v in positive.items():
-        if target_user in v:
-            targetNum += 1
-            for i in range(len(v)):
-                c[(target_user, i)] += 1
-    
-    for i in c.keys():
-        tmp = 0
-        for j in c.keys():
-            if i[1] in j:
-                tmp += c[j]
-        
-        similarity[i] = c[i] / (targetNum * tmp) ** 0.5
 
-    print(len(set(similarity.values())))
+
+
+#    with open('positive.json', 'r', encoding='utf-8') as f:
+#        positive = json.loads(f.read())
+#    
+#    similarity, c  = defaultdict(int), defaultdict(int)
 #    target_user = 19
-#    all_users = rating.user_id.unique().tolist()
-#    for user in all_users[:100]:
-#        cuser, ctarget_user, both = 0, 0, 0
-#        for k,v in positive.items():
-#            if user in v:
-#                cuser += 1
-#            if target_user in v:
-#                ctarget_user += 1
-#            if user in v and target_user in v:
-#                both += 1
+#    targetNum = 0
+#    for k,v in positive.items():
+#        if target_user in v:
+#            targetNum += 1
+#            for i in v:
+#                c[i] += 1
 #
-#        if cuser != 0 and ctarget_user != 0:    
-#            similarity[(target_user, user)] = both / (cuser * ctarget_user) ** 0.5
-#
-#    print([i[0][1] for i in sorted(similarity.items(), key = lambda x: x[1], reverse = True)])
+#    print("hello")    
+#    for i in c.keys():
+#        new_rating = rating[rating.user_id == i]
+#        tmp = len(new_rating[new_rating.rating > 5])
+##        print(i, tmp)
+#        similarity[i] = c[i] / (targetNum * tmp) ** 0.5
+#    
+#    print("hello")
+##    sort = sorted(similarity.items(), key = lambda x: x[1], reverse = True)
+#    with open('similarity.json', 'w', encoding='utf-8') as f:
+#        f.write(json.dumps(similarity))
+    with open('similarity.json', 'r', encoding='utf-8') as f:
+        similarity = json.loads(f.read())
+
+    sort = sorted(similarity.items(), key = lambda x: x[1], reverse = True)
+    similist = [sort[i][0] for i in range(1, 6)]
+    
+    likeAnime = []
+    target_user = 19
+    for u in similist:
+        new_rating = rating[rating.user_id == int(u)]
+        tmp = new_rating[new_rating.rating > 5].anime_id.tolist()
+        likeAnime += tmp        
+
+    targetAnime = set(rating[rating.user_id == target_user].anime_id.tolist())
+    reclist = set(likeAnime) & (targetAnime ^ set(likeAnime))
+    
+    recName = []
+    for i in reclist:
+        recName += anime[anime.anime_id == i].name.tolist()
+
+    print(recName)
